3bda2fe240888b784dc93db7f69f757906a67f1f
Danilo de Oliveira <danilo.psn94@gmail.com>
2016-10-23 13:37:42 -0200

    Refatoração do Código

--- Linguagens_Livre_Do_Contexto/LFA-ReconhecedorLLC-Cmy/Gramatica.py
+++ Linguagens_Livre_Do_Contexto/LFA-ReconhecedorLLC-Cmy/Gramatica.py
@@ -2,7 +2,7 @@
 class Gramatica:
 
     def __init__(self):
-        self.dicionario_producoes = dict()
+        self.producoes = dict()
         self.terminais = set()
         self.variaveis = set()
         self.variavel_partida = str()
@@ -13,16 +13,16 @@ class Gramatica:
         str_producao = str_producao.replace(" ", "").replace("\n", "")
 
         # Define a primeira variavel das producoes como variavel de partida.
-        if len(self.dicionario_producoes) == 0:
+        if len(self.producoes) == 0:
             self.variavel_partida = str_producao[0]
 
         variavel = str_producao.split("->")[0]
         producao = str_producao.split("->")[1]
 
-        if not( self.dicionario_producoes.__contains__(variavel) ):
-            self.dicionario_producoes[variavel] = set()
+        if not(self.producoes.__contains__(variavel)):
+            self.producoes[variavel] = set()
 
-        self.dicionario_producoes[variavel].add(producao)
+        self.producoes[variavel].add(producao)
         self.variaveis.add( variavel )
 
         if len(producao) == 1:
@@ -33,14 +33,14 @@ class Gramatica:
         print( "Variaveis: " + str(self.variaveis) )
         print( "Terminais: " + str(self.terminais) )
         print( "Variavel de partida: " + self.variavel_partida )
-        print( "Producoes: " + str(self.dicionario_producoes) )
+        print( "Producoes: " + str(self.producoes))
 
     # Retorna o lado esquerdo da producao a partir uma das producoes do lado direito
     def get_left_side_production(self, str_right_side_production):
         set_left_side_production = set()
 
         for variavel in self.variaveis:
-            if str_right_side_production in self.dicionario_producoes[ variavel ]:
+            if str_right_side_production in self.producoes[ variavel]:
                 set_left_side_production.add( variavel )
 
         return set_left_side_production
